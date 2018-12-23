df8be099b1c74dbcc16d6fbaadac540218a298bd
jasonserviss <jason.serviss@ki.se>
2018-12-23 12:50:44 +0100

    adding runAnalysis.R script

---
+++ analysis/runAnalysis.R
@@ -0,0 +1,3 @@
+#SCM.analysis
+source('/home/Github/CIMseq.testing/analysis/SCM.analysis/scripts/runCountsSorted2.R')
+rmarkdown::render('/home/Github/CIMseq.testing/analysis/SCM.analysis/analysis/analysis.Rmd')
