b8c9569c9ed314db10e3ed35c50ac422f0393c12
jasonserviss <jason.serviss@ki.se>
2018-12-24 13:42:54 +0100

    updating visualizingSolutionSpace script and analysis

--- analysis/visualizingSolutionSpace/scripts/script.R
+++ analysis/visualizingSolutionSpace/scripts/script.R
@@ -1,53 +1,40 @@
 packages <- c(
-  "sp.scRNAseqData", "sp.scRNAseq", "seqTools",
-  "printr", "ggthemes", "tidyverse", "future"
+  "CIMseq.data", "CIMseq", "printr", "ggthemes", "tibble", "future",
+  "purrr", "dplyr", "tidyr"
 )
 purrr::walk(packages, library, character.only = TRUE)
 rm(packages)
 
 currentDir <- getwd()
 
-s <- str_detect(colnames(countsSorted2), "^s")
-sng <- countsSorted2[, s]
-mul <- countsSorted2[, !s]
-cObjSng <- spCounts(sng, countsSortedERCC2[, s])
-bool <- colnames(mul) %in% c("m.NJB00204.G04", "m.NJB00204.D07", "m.NJB00204.F12")
-cObjMul <- spCounts(mul[, bool], countsSortedERCC2[, !s][, bool])
-uObj <- spUnsupervised(cObjSng)
+load('~/Github/CIMseq.testing/analysis/SCM.analysis/data/CIMseqData.rda')
+keep <- c("m.NJB00204.G04", "m.NJB00204.D07", "m.NJB00204.F12")
+cObjMul <- CIMseqMultiplets(
+  getData(cObjMul, "counts")[, keep],
+  getData(cObjMul, "counts.ercc")[, keep],
+  getData(cObjMul, "features")
+)
+
 plan(multiprocess)
-sObj <- spSwarm(
-  cObjSng, cObjMul, uObj, maxiter = 40, swarmsize = 200, 
+sObj <- CIMseqSwarm(
+  cObjSng, cObjMul, maxiter = 40, swarmsize = 200,
   nSyntheticMultiplets = 500, report = TRUE, reportRate = 1
 )
 
-ss <- getData(sObj, "spSwarm")
+ss <- getData(sObj, "fractions")
 colnames(ss) <- c("HOS", "HCT116", "A375")
-sObj@spSwarm <- ss
+sObj@fractions <- ss
 
-save(cObjSng, cObjMul, uObj, sObj, file = file.path(currentDir, 'data/algoOutput.rda'))
+save(cObjSng, cObjMul, sObj, file = file.path(currentDir, 'data/algoOutput.rda'))
 
-processReports <- function(sObj) {
-  tibble(
-    sample = rownames(getData(sObj, "spSwarm")),
-    iteration = map(getData(sObj, "stats"), function(x) x$it),
-    error = map(getData(sObj, "stats"), function(x) x$error),
-    fitness = map(getData(sObj, "stats"), function(x) x$f),
-    position = map(getData(sObj, "stats"), function(x) {
-      map(x$x, function(y) t(y) * 1/colSums(y))
-    })
-  ) %>% 
-    unnest() %>%
-    mutate(position = map(position, function(x) {
-      x %>% 
-        as.data.frame() %>% 
-        setNames(colnames(getData(sObj, "spSwarm"))) %>% 
-        as_tibble() %>%
-        add_column(swarmMemberID = 1:nrow(.), .before = 1) %>%
-        ungroup() 
-    }))
+renameClasses <- function(class) {
+  case_when(
+    class == "0" ~ "A375", class == "1" ~ "HCT116", class == "2" ~ "HOS",
+    TRUE ~ "error"
+  )
 }
-
-report <- processReports(sObj)
+report <- unnest(getData(sObj, "stats"))
+colnames(report)[6:8] <- renameClasses(colnames(report)[6:8])
 
 save(report, file = file.path(currentDir, "data/report.rda"))
 writeLines(capture.output(sessionInfo()), "logs/sessionInfo.txt")
