7e54f89b894fb8a0eae114c4e58b12f95a52bc91
jasonserviss <jason.serviss@ki.se>
2018-12-26 13:05:35 +0100

    more debugging again

--- inst/analysis/runAnalysis.R
+++ inst/analysis/runAnalysis.R
@@ -9,8 +9,9 @@ directories <- file.path(baseDir, analysisDirs)
 for(i in 1:length(directories)) {
   print(paste0("Processing ", basename(directories[i])))
   setwd(directories[i])
-  source(file.path(directories[i], 'scripts/script.R'))
+  print(paste0("Working dir ", getwd()))
   print(list.files(directories[i]))
+  if(file.exists('./scripts/script.R')) source(file.path(directories[i], 'scripts/script.R'))
   rmarkdown::render(file.path(directories[i], 'analysis/analysis.Rmd'))
   setwd('~/Github/CIMseq.testing')
 }
