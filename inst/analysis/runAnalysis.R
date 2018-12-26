3751e752791ab0d7dd8e4c25e71cb25b5397dd8c
jasonserviss <jason.serviss@ki.se>
2018-12-26 16:04:45 +0100

    more debugging again

--- inst/analysis/runAnalysis.R
+++ inst/analysis/runAnalysis.R
@@ -1,10 +1,8 @@
 
-baseDir <- '~/Github/CIMseq.testing/inst/analysis'
-analysisDirs <- c(
+directories <- file.path(baseDir, c(
   "SCM.analysis", "visualizingPoissonAlgo", "visualizingSolutionSpace",
   "syntheticMultipletsFromCounts"
-)
-directories <- file.path(baseDir, analysisDirs)
+))
 
 for(i in 1:length(directories)) {
   print(paste0("Processing ", basename(directories[i])))
