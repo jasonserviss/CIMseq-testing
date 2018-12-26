ec773e4d573bd298e53d179e76f38408406aa8c8
jasonserviss <jason.serviss@ki.se>
2018-12-26 11:58:13 +0100

    runAnalysis.R bugs again

--- inst/analysis/runAnalysis.R
+++ inst/analysis/runAnalysis.R
@@ -1,12 +1,13 @@
 
-baseDir <- 'analysis'
+baseDir <- '~/Github/CIMseq.testing/inst/analysis'
 analysisDirs <- c(
   "SCM.analysis", "visualizingPoissonAlgo", "visualizingSolutionSpace",
   "syntheticMultipletsFromCounts"
 )
-directories <- system.file(file.path(baseDir, analysisDirs), package = 'CIMseq.testing')
+directories <- file.path(baseDir, analysisDirs)
 
 for(i in 1:length(directories)) {
+  
   source(file.path(directories[i], 'scripts/script.R'))
   rmarkdown::render(file.path(directories[i], 'analysis/analysis.Rmd'))
 }
