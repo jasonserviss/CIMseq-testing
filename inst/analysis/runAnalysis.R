503d5bd1fced9573936a557f37718eabf24cb836
jasonserviss <jason.serviss@ki.se>
2018-12-26 09:40:57 +0100

    runAnalysis.R bug fix

--- inst/analysis/runAnalysis.R
+++ inst/analysis/runAnalysis.R
@@ -1,9 +1,9 @@
-
+baseDirs <- '~/Github/CIMseq.testing/inst/analysis'
 analysisDirs <- c(
   "SCM.analysis", "visualizingPoissonAlgo", "visualizingSolutionSpace",
   "syntheticMultipletsFromCounts"
 )
-
+fullPaths <- file.path(baseDirs, analysisDirs)
 for(i in 1:length(analysisDirs)) {
   setwd(analysisDirs[i])
   source('./scripts/script.R')
